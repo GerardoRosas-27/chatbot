589797cec46f12aba8ef5401b189f38899609518
Gerardo Gabriel Rosas Rodríguez <grosas@gdc.ad.com.mx>
2019-12-14 19:27:17 -0600

    actualizar

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-'use strict';
+
 var express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 const Chapi = require('whatsapp-chapi');
 const inst = "https://eu34.chat-api.com/instance86436";
 const token = "hjzo5kodgia081x5";
 const phone = "5217561019626";
 //const url = "https://chatbot.herokuapp.com/";
-const url = "https://chatbotchapi.herokuapp.com/";
+const webhookUrl = "https://chatbotchapi.herokuapp.com/";
 
 
 const app = express();
 //app.use(url, bot.middleware());
-const bot = new Chapi(inst, token, url);
+const bot = new Chapi(inst, token, webhookUrl);
 
 bot.signIn(phone)
 .then(() => yourBot.doSomething())
@@ -20,9 +21,7 @@ bot.signIn(phone)
 app.use(morgan('dev'));
 
 
-
-
-app.post(url,  function (req, res) {
+app.post(webhookUrl,  function (req, res) {
     bot.sendMessage(req.body.messages[0].author, 'ECHO TEXT');
     console.log(req.body);
     res.sendStatus(200);
@@ -33,5 +32,4 @@ const http = require('http');
 const port = process.env.PORT || 4000;
  
 // Chapi will push messages sent to this URL. Web server should be internet-facing.
-const webhookUrl = process.env.WEBHOOK_URL;
 http.createServer(app).listen(port, () => bot.setWebhook(webhookUrl));
