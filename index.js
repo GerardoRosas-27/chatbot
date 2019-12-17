e5a3b947f7ec6353b1ee2af6c77d7695743ca40a
Gerardo Gabriel Rosas Rodríguez <grosas@gdc.ad.com.mx>
2019-12-17 17:30:30 -0600

    asd

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const enviar = require("./modelChat");
+var fs = require("fs");
+
 
 const restService = express();
 
@@ -13,12 +15,68 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/chat",async function(req, res) {
-  const datamensaje = {
-    "phone": "5215534122853",
-    "body": "resultados" + req.body
-}
-const result = await enviar.post('https://eu97.chat-api.com/instance86894/sendMessage?token=90jjwn8m91ttl8un', datamensaje);
-  return res.json(req.body);
+    var result = req.body.messages[0];
+   
+    var mensaje = result.body;
+
+    var autor = result.author; 
+    var corte1 = autor.indexOf("@");
+     var telefono = autor.substr(0,corte1);
+    console.log(telefono);
+    var entro = false;
+    switch (mensaje) {
+        case "hola":
+           var datamensaje = {
+                "phone": telefono,
+                "body": "para el menu 1 = 1 || para el menu 2 = 2 || salir = salir"
+            }
+            var entro = true;
+            break;
+        case "1":
+            var datamensaje = {
+                "phone": telefono,
+                "body": "entrastes al menu 1"
+            }
+            var entro = true;
+           break;  
+        case "2":
+            var datamensaje = {
+                "phone": telefono,
+                "body": "entrastes al menu 2"
+            }
+           var entro = true;
+           break; 
+           case "salir":
+            var datamensaje = {
+                "phone": telefono,
+                "body": "Grasias, si quieres entrar de nuevo escribe hola"
+            }
+           var entro = true;
+           break;  
+        default:
+            break;
+    }
+
+  
+    if(entro){
+        const result = await enviar.post('https://eu97.chat-api.com/instance86894/sendMessage?token=90jjwn8m91ttl8un', datamensaje);
+    }
+return res.json(req.body);
+});
+
+
+restService.post("/corte",async function(req, res) {
+    var contents = fs.readFileSync("usuario.json");
+    // Define to JSON type
+    var mensajes = JSON.parse(contents);
+    var result = mensajes.messages[0];
+    console.log(mensajes.messages[0]);
+    var autor = result.author;
+    
+    var corte1=autor.indexOf("@");
+     var telefono =autor.substr(0,corte1);
+    console.log(telefono);
+return res.json(req.body);
 });
 
 restService.listen(process.env.PORT || 8000, function() {
